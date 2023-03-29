78b202d0c22b62d6752376fa97b38034f26d9d93
eliyahukoren <elik.koren@gmail.com>
2023-03-29 12:16:13 +0300

    example of how to work with not serializable data, update action

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,18 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import { Action, configureStore, getDefaultMiddleware, ThunkAction } from '@reduxjs/toolkit';
 import todoReducer from '../features/todo/todoSlice';
 
 export const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "todos/updatedTodo/fulfilled",
+        ],
+      },
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
