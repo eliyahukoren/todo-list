9dc71eabde2bc724e01c753f7e1f5c78ce037f39
eliyahukoren <elik.koren@gmail.com>
2023-04-01 09:19:25 +0300

    add loading status and refactor show todos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,16 @@ const App: React.FC = () => {
   const store = useAppSelector((state) => state.todos);
   const dispatch = useAppDispatch();
 
+  const showTodos = (): JSX.Element | JSX.Element[] => {
+    if( store.todos.length === 0){
+      return <h2>No Todos</h2>
+    }
+
+    return store.todos.map(
+      (todo: ITodo) => <TodoItem key={todo.id} todo={todo} />
+    )
+  }
+
   useEffect(() => {
     dispatch(fetchAllTodos());
   }, []);
@@ -17,13 +27,10 @@ const App: React.FC = () => {
   return (
     <main className='app'>
       <h1>Todo List</h1>
+      <h4>{store.isLoading && `Loading ...`}</h4>
       <AddTodo />
       <FilterTodo />
-      {
-        store.todos.map((todo:ITodo) => (
-          <TodoItem key={todo.id} todo={todo}/>
-        ))
-      }
+      { showTodos() }
     </main>
   );
 }
